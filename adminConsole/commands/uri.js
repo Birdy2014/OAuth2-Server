a097ced37d1730e86d8b2e0a391299bc1fa8b428
Birdy2014 <moritzv7@googlemail.com>
2019-05-25 22:28:23 +0200

    Add uri remove and get commands

--- adminConsole/commands/uri.js
+++ adminConsole/commands/uri.js
@@ -38,11 +38,39 @@ module.exports.run = async args => {
             break;
         }
         case "remove": {
-            //TODO
+            args.shift();
+            try {
+                if (args.length < 2) {
+                    console.log("Usage: uri remove <client name or ID> <redirect_uri>");
+                } else {
+                    await getClientId(args[0], async client_uri => {
+                        await UriMethods.removeUri(client_uri, args[1]);
+                        console.log("Removed redirect_uri");
+                    });                  
+                }
+            } catch(e) {
+                console.error(e);
+            }
             break;
         }
         case "get": {
-            //TODO
+            args.shift();
+            try {
+                if (args.length === 0) {
+                    console.log("Usage: uri get <client name or ID>");
+                } else {
+                    await getClientId(args[0], async client_id => {
+                        let results = await dbInterface.query(`SELECT redirect_uri.redirect_uri AS redirect_uri, client.name AS name, user.email AS email, user.username AS username FROM redirect_uri JOIN client ON redirect_uri.client_id = client.client_id JOIN user ON client.dev_id = user.user_id WHERE redirect_uri.client_id = '${client_id}'`);
+                        let output = "";
+                        for (const result of results) {
+                            output += `client_id: ${client_id} client_name: ${result.name} redirect_uri: ${result.redirect_uri} dev_email: ${result.email} dev_name: ${result.username}\n`;
+                        }
+                        console.log(output.substring(0, output.length -1));
+                    });
+                }
+            } catch(e) {
+                console.error(e);
+            }
             break;
         }
         default: {
