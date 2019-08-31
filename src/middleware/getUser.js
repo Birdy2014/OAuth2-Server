3d34dd7eaf28d17ee8263f9517d6345c1dcf40e6
Birdy2014 <moritzv7@googlemail.com>
2019-08-31 18:54:44 +0200

    fix: admin permissions are checked corectly

--- src/middleware/getUser.js
+++ src/middleware/getUser.js
@@ -14,7 +14,7 @@ async function getUser(req, res, next) {
         let active = results.length === 1 && results[0].expires > currentUnixTime();
         if (active) {
             req.client = { client_id: results[0].client_id, name: results[0].client_name, origin: "access_token" }
-            req.user = { user_id: results[0].user_id, username: results[0].username, email: results[0].email, permissions: await getPermissions(results[0].user_id, results[0].client_id), admin: hasPermission(results[0].user_id, await dbInterface.getDashboardId(), "admin"), origin: "access_token" };
+            req.user = { user_id: results[0].user_id, username: results[0].username, email: results[0].email, permissions: await getPermissions(results[0].user_id, results[0].client_id), admin: await hasPermission(results[0].user_id, await dbInterface.getDashboardId(), "admin"), origin: "access_token" };
         } else {
             respond(res, 403, undefined, "Invalid access_token");
             return;
@@ -25,7 +25,7 @@ async function getUser(req, res, next) {
         let results = await dbInterface.query(`SELECT refresh_token.user_id AS user_id, user.username AS username, user.email AS email, refresh_token.client_id AS client_id, client.name AS client_name FROM refresh_token JOIN user ON refresh_token.user_id = user.user_id JOIN client ON refresh_token.client_id = client.client_id WHERE refresh_token = '${refresh_token}'`);
         if (results.length === 1) {
             req.client = { client_id: results[0].client_id, name: results[0].client_name, origin: "refresh_token" }
-            req.user = { user_id: results[0].user_id, username: results[0].username, email: results[0].email, permissions: await getPermissions(results[0].user_id, results[0].client_id), admin: hasPermission(results[0].user_id, await dbInterface.getDashboardId(), "admin"), origin: "refresh_token" };
+            req.user = { user_id: results[0].user_id, username: results[0].username, email: results[0].email, permissions: await getPermissions(results[0].user_id, results[0].client_id), admin: await hasPermission(results[0].user_id, await dbInterface.getDashboardId(), "admin"), origin: "refresh_token" };
         } else {
             respond(res, 403, undefined, "Invalid refresh_token");
             return;
@@ -36,7 +36,7 @@ async function getUser(req, res, next) {
         let results = await dbInterface.query(`SELECT authorization_code.user_id AS user_id, user.username AS username, user.email AS email, authorization_code.client_id AS client_id, client.name AS client_name FROM authorization_code JOIN user ON authorization_code.user_id = user.user_id JOIN client ON authorization_code.client_id = client.client_id WHERE authorization_code = '${authorization_code}'`);
         if (results.length === 1) {
             req.client = { client_id: results[0].client_id, name: results[0].client_name, origin: "authorization_code" }
-            req.user = { user_id: results[0].user_id, username: results[0].username, email: results[0].email, permissions: await getPermissions(results[0].user_id, results[0].client_id), admin: hasPermission(results[0].user_id, await dbInterface.getDashboardId(), "admin"), origin: "authorization_code" };
+            req.user = { user_id: results[0].user_id, username: results[0].username, email: results[0].email, permissions: await getPermissions(results[0].user_id, results[0].client_id), admin: await hasPermission(results[0].user_id, await dbInterface.getDashboardId(), "admin"), origin: "authorization_code" };
         } else {
             respond(res, 403, undefined, "Invalid authorization_code");
             return;
@@ -47,7 +47,7 @@ async function getUser(req, res, next) {
         if (user_id) {
             let results = await dbInterface.query(`SELECT username, email FROM user WHERE user_id = '${user_id}'`);
             if (results.length === 1)
-                req.user = { user_id: user_id, username: results[0].username, email: results[0].email, admin: hasPermission(results[0].user_id, await dbInterface.getDashboardId(), "admin"), origin: "basic" }
+                req.user = { user_id: user_id, username: results[0].username, email: results[0].email, admin: await hasPermission(results[0].user_id, await dbInterface.getDashboardId(), "admin"), origin: "basic" }
         } else {
             respond(res, 403, undefined, "Invalid user credentials");
             return;
