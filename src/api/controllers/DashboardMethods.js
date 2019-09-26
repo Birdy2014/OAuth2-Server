37c1ea00601e551dd8a8c325fa6fe16d1ea1d157
Birdy2014 <moritzv7@googlemail.com>
2019-09-26 16:52:40 +0200

    feat(dashboard): additional user info can be changed

--- src/api/controllers/DashboardMethods.js
+++ src/api/controllers/DashboardMethods.js
@@ -1,17 +1,16 @@
 const { respond, handleError } = require("../utils");
+const configReader = new (require("../../ConfigReader"))();
 
 function get(req, res) {
     try {
         if (req.client.name !== "Dashboard" || req.client.origin !== "access_token")
             throw { status: 400, error: "Invalid arguments" };
 
-        respond(res, 200, {
-            user_id: req.user.user_id,
-            username: req.user.username,
-            email: req.user.email,
-            permissions: req.user.permissions,
-            admin: req.user.admin
-        });
+        let response = {};
+        response.user = req.user;
+        delete response.user.origin;
+        response.user_info = configReader.config.user_info;
+        respond(res, 200, response);
     } catch (e) {
         handleError(res, e);
     }
--- src/public/js/dashboard.js
+++ src/public/js/dashboard.js
@@ -43,14 +43,72 @@ async function refreshToken(refresh_token) {
     return tokens.data.access_token;
 }
 
+function createInputField(name, text, options) {
+    let container = document.createElement("div");
+    let input;
+    container.innerHTML = text;
+    if (options && options.length > 0) {
+        input = document.createElement("select");
+        for (const option of options) {
+            let e = document.createElement("option");
+            e.value = option;
+            e.innerHTML = option;
+            input.append(e);
+        }
+    } else {
+        input = document.createElement("input");
+        input.type = "text";
+    }
+    input.name = name;
+    input.className = "inputs_user_info";
+    container.append(input);
+    return container;
+}
+
 async function loadData(access_token) {
     try {
+        settings_input_container.innerHTML = "";
+
         //Get connected clients, user information, permissions
-        let { user_id, username, email, permissions, admin } = JSON.parse(await request(`${url.protocol}//${url.host}/api/dashboard`, "GET", "", access_token)).data;
+        let admin = false;
+        let { user, user_info } = JSON.parse(await request(`${url.protocol}//${url.host}/api/dashboard`, "GET", "", access_token)).data;
 
         //Load data to DOM
-        input_username.placeholder = username;
-        input_email.placeholder = email;
+        settings_input_container.append(createInputField("username", "Username: "));
+        settings_input_container.append(createInputField("email", "Email Adresse: "));
+        for (const key in user_info) {
+            let field = createInputField(key, `${key}: `, user_info[key]);
+            settings_input_container.append(field);
+        }
+
+        inputs_user_info = Array.from(document.getElementsByClassName("inputs_user_info"));
+
+        for (const key in user) {
+            switch(key) {
+                case "user_id":
+                    break;
+                case "permissions":
+                    break;
+                case "admin":
+                    admin = user[key];
+                    break;
+                case "verified":
+                    if (!user[key]) alert("Please verify your email address");
+                    break;
+                default:
+                    let e = inputs_user_info.filter(value => value.name === key)[0];
+                    if (e && e.tagName.toLowerCase() === "input") {
+                        e.placeholder = user[key];
+                    } else if (e && e.tagName.toLowerCase() === "select") {
+                        for (const option of Array.from(e.childNodes)) {
+                            if (option.value === user[key]) {
+                                option.setAttribute("selected", "");
+                                break;
+                            }
+                        }
+                    }
+            }
+        }
 
         //admin settings
         if (admin) {
@@ -58,7 +116,7 @@ async function loadData(access_token) {
             let users = JSON.parse(await request(`${url.protocol}//${url.host}/api/user`, "GET", "", access_token)).data;
             users.forEach(user => {
                 let user_element = document.createElement("li");
-                user_element.innerHTML = `user_id: ${user.user_id}, username: <input type="text" placeholder="${user.username}" id="input_username_${user.user_id}"/>, email: <input type="email" placeholder="${user.email}" id="input_email_${user.user_id}"/>, Password: <input type="password" id="input_password_${user.user_id}"/>, verified: ${user.verified}, admin: ${user.admin} <a href="javascript:void(0)" onclick="changeSettings(global_access_token, '${user.user_id}', document.getElementById('input_username_${user.user_id}').value, document.getElementById('input_email_${user.user_id}').value, document.getElementById('input_password_${user.user_id}').value)">Submit</a>`;
+                user_element.innerHTML = `user_id: ${user.user_id}, username: <input type="text" placeholder="${user.username}" id="input_username_${user.user_id}"/>, email: <input type="email" placeholder="${user.email}" id="input_email_${user.user_id}"/>, Password: <input type="password" id="input_password_${user.user_id}"/>, verified: ${user.verified}, admin: ${user.admin} <a href="javascript:void(0)" onclick="changeSettings(global_access_token, '${user.user_id}', { username: document.getElementById('input_username_${user.user_id}').value, email: document.getElementById('input_email_${user.user_id}').value, password: document.getElementById('input_password_${user.user_id}' }).value)">Submit</a>`;
                 user_list.appendChild(user_element);
             });
             let clients = JSON.parse(await request(`${url.protocol}//${url.host}/api/client`, "GET", "", access_token)).data;
@@ -69,6 +127,7 @@ async function loadData(access_token) {
             });
         }
     } catch (e) {
+        console.error(e);
         if (JSON.parse(e).status === 403) {
             global_access_token = await refreshToken(window.localStorage.getItem("refresh_token"));
             await loadData(global_access_token);
@@ -78,19 +137,21 @@ async function loadData(access_token) {
     }
 }
 
-async function changeSettings(access_token, user_id, new_username, new_email, new_password) {
+async function changeSettings(access_token, user_id, settings) {
     try {
-        if (!(new_username || new_email || new_password)) return { status: 400, error: "Input missing" };
         let data = "";
         if (user_id) data += `user_id=${user_id}&`;
-        if (new_username) data += `username=${new_username}&`;
-        if (new_email) data += `email=${new_email}&`;
-        if (new_password) data += `password=${new_password}&`;
+        for (const setting in settings) {
+            if (settings[setting])
+                data += setting + "=" + settings[setting] + "&";
+        }
+        if (data === "") throw { status: 400, error: "Input missing" };
         data = data.substring(0, data.length - 1);
 
         let result = JSON.parse(await request(`${url.protocol}//${url.host}/api/user`, "PUT", data, access_token));
         return result;
     } catch (e) {
+        console.error(e);
         e = JSON.parse(e);
         if (e.status === 403) {
             await changeSettings(await refreshToken(window.localStorage.getItem("refresh_token")), user_id, new_username, new_email, new_password);
@@ -121,17 +182,16 @@ async function logout() {
 
 const url = new URL(window.location);
 var global_access_token = "";
-var input_username;
-var input_email;
 var input_password;
 var input_confirm_password;
+var inputs_user_info = [];
+var settings_input_container;
 var user_list;
 var client_list;
 window.onload = () => {
-    input_username = document.getElementById("input_username");
-    input_email = document.getElementById("input_email");
     input_password = document.getElementById("input_password");
     input_confirm_password = document.getElementById("input_confirm_password");
+    settings_input_container = document.getElementById("settings_input_container");
     user_list = document.getElementById("user_list");
     client_list = document.getElementById("client_list");
 
@@ -141,10 +201,13 @@ window.onload = () => {
         if (input_password.value !== input_confirm_password.value) {
             alert("Passwords don't match");
         } else {
-            changeSettings(global_access_token, undefined, input_username.value, input_email.value, input_password.value).then(result => {
+            let settings = {};
+            for (const input of inputs_user_info) {
+                if (input)
+                    settings[input.name] = input.value;
+            }
+            changeSettings(global_access_token, undefined, settings).then(result => {
                 if (result.status === 200) {
-                    input_username.value = "";
-                    input_email.value = "";
                     input_password.value = "";
                     input_confirm_password.value = "";
                     loadData(global_access_token);
